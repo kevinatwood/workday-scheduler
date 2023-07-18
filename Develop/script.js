9effebcc0d2991ba1a229eba9c3290a49124bcaa
Kevin Atwood <atwoodkevin22@gmail.com>
2023-07-18 11:35:42 -0500

    Added listener to the save button to save the user's events. Applied past, present, and future classes to each time block by comparing the id to the current hour. Added function to get user input from local storage and display it on page. Added function displaying the current date on the top of the page. Debugged and seems to be working as intended.

--- Develop/script.js
+++ Develop/script.js
@@ -2,22 +2,51 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+  //  Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // local storage. 
+  function saveEvent(event) {
+    var textarea = $(this).siblings(".description").val()
+    var timeId = $(this).parent().attr("id")
+    localStorage.setItem(timeId, textarea)
+  }
+  var saveBtn = $('.saveBtn');
+  saveBtn.on("click", saveEvent)
+
+  // Apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. 
+
+function applyTime(){
+  var currentHour = dayjs().hour()
+  
+  for (var i = 9; i < 18; i++){
+    var timeBlock = $("#hour-" + [i]);
+    if ([i] < currentHour){
+      timeBlock.attr('class', 'row time-block past')
+    } else if ([i] == currentHour){
+      timeBlock.attr('class' , 'row time-block present')
+    } else {
+      timeBlock.attr ('class', 'row time-block future')
+    }
+  }
+
+}
+  //  get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. 
+  function getEvent(){
+    for (var i = 9; i < 18; i++)
+   $("#hour-"+[i] + " .description").val(localStorage.getItem("hour-"+[i]))
+  }
+
+  //code to display the current date in the header of the page.
+
+  function displayDate(){
+    currentDate = dayjs().format("MM/DD/YYYY")
+    currentDateEl = $('#currentDay')
+    currentDateEl.text("The date today is " + currentDate)
+  }
+ displayDate()
+ getEvent()
+ applyTime()
 });
+
